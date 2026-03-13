bbe19b0faa8cee259a0e24160de8a899af76c9ee
TanPLQ <phamlequoctan@gmail.com>
2026-03-14 01:29:36 +0700

    add: test data csv file

--- backend-spring/petties/src/test/java/com/petties/petties/service/ClinicServiceServiceUnitTest.java
+++ backend-spring/petties/src/test/java/com/petties/petties/service/ClinicServiceServiceUnitTest.java
@@ -409,10 +409,11 @@ class ClinicServiceServiceUnitTest {
         when(authService.getCurrentUser()).thenReturn(owner);
         when(clinicRepository.findFirstByOwnerUserId(owner.getUserId())).thenReturn(Optional.of(clinic));
         when(clinicServiceRepository.findByServiceIdAndClinic(serviceId, clinic)).thenReturn(Optional.of(service));
-        doNothing().when(clinicServiceRepository).delete(service);
+        when(clinicServiceRepository.save(service)).thenReturn(service);
 
         assertDoesNotThrow(() -> clinicServiceService.deleteService(serviceId));
-        verify(clinicServiceRepository).delete(service);
+        verify(clinicServiceRepository).save(service);
+        assertFalse(service.getIsActive());
     }
 
     private ClinicService createService(String name, boolean isHomeVisit, VaccineTemplate template) {
